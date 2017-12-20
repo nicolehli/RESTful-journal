f90535a42201c42b0e5e2c7e196dc8d7084e3038
Nicole Li <nicolehcli@gmail.com>
2017-12-20 21:25:51 +0000

    Index Route

--- app.js
+++ app.js
@@ -21,11 +21,35 @@ var blogSchema  = new mongoose.Schema({
 })
 var Blog = mongoose.model("Blog", blogSchema)
 
+// Test Blog
+// Blog.create({
+//     title:  "Test Blog",
+//     image:  "https://images.pexels.com/photos/176381/pexels-photo-176381.jpeg?w=940&h=650&auto=compress&cs=tinysrgb",
+//     body:   "Hello World, this is the blog body.",
+// })
+
 // RESTFUL ROUTES
+
+app.get("/", function(req,res){
+    res.redirect("/blogs")
+})
+
+
+app.get("/blogs", function(req,res){
+    Blog.find({}, function(err, foundBlogs){
+        if(err){
+            console.log(err)
+        } else { 
+            res.render("index", {blogs: foundBlogs})
+        }
+    })
+
+})
+
 
 // PORT SETUP
 var port        = process.env.PORT | 3000;
-app.listen(port, process.env.IP, function(){ 
+app.listen(process.env.PORT, process.env.ID, function(){ 
     console.log("SERVER IS RUNNING")
 })
 
